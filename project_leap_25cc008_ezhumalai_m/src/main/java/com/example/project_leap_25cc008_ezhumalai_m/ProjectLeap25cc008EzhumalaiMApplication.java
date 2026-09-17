8e42c7ec592ca5f13b319dfdd8f17eb842de8079
ezhumalaim2025cce-cpu <ezhumalai.m2025cce@sece.ac.in>
2026-09-17 15:56:49 +0530

    Add files via upload

--- project_leap_25cc008_ezhumalai_m/src/main/java/com/example/project_leap_25cc008_ezhumalai_m/ProjectLeap25cc008EzhumalaiMApplication.java
+++ project_leap_25cc008_ezhumalai_m/src/main/java/com/example/project_leap_25cc008_ezhumalai_m/ProjectLeap25cc008EzhumalaiMApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectLeap25cc008EzhumalaiMApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ProjectLeap25cc008EzhumalaiMApplication.class, args);
-
 	}
 
 }
