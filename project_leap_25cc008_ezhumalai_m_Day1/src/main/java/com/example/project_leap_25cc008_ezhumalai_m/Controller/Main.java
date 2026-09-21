467c87db80ad5822685a2e81722e9e9f77499ba1
ezhumalaim2025cce-cpu <ezhumalai.m2025cce@sece.ac.in>
2026-09-21 09:32:35 +0530

    Add files via upload

---
+++ project_leap_25cc008_ezhumalai_m_Day1/src/main/java/com/example/project_leap_25cc008_ezhumalai_m/Controller/Main.java
@@ -0,0 +1,8 @@
+package com.example.project_leap_25cc008_ezhumalai_m.Controller;
+
+public class Main {
+    static void main(){
+        WebController webcontroller = new WebController();
+        IO.print(webcontroller.calculate(10 , 20 , "+"));
+    }
+}
